62c07f0337853932ae19f5cb3419a4e3bbc43b00
Sakada-Ly <sakada.ly@edifixio.com>
2021-08-31 20:09:15 +0200

    add test for creating model

--- provider/models.py
+++ provider/models.py
@@ -28,11 +28,11 @@ class ProviderAvailibility(DateModification):
   Availibility of a provider connexion given a coordinate
   """
 
-  operateur = models.CharField(max_length=5)
+  provider_code = models.CharField(max_length=5)
   lamb_x_coord = models.IntegerField()
   lamb_y_coord = models.IntegerField()
-  gps_x_coord = models.DecimalField(max_digits=18, decimal_places=16, blank=True, null=True)
-  gps_y_coord = models.DecimalField(max_digits=18, decimal_places=16, blank=True, null=True)
+  gps_x_coord = models.DecimalField(max_digits=18, decimal_places=15, blank=True, null=True)
+  gps_y_coord = models.DecimalField(max_digits=18, decimal_places=15, blank=True, null=True)
   index_lamb_coord = models.CharField(max_length=255, blank=True, null=True)
   availibility_2G = models.BooleanField(default=False, verbose_name="2G availibility")
   availibility_3G = models.BooleanField(default=False, verbose_name="3G availibility")
@@ -41,12 +41,12 @@ class ProviderAvailibility(DateModification):
   def save(self, *args, **kwargs):
     # Fill gps coordinate if needed
     if(not self.gps_x_coord or not self.gps_y_coordinate):
-      point = convert_lambert_to_gps(self.x_lamb_coord, self.y_lamb_coord)
-      self.gps_x_coord = point.x
-      self.gps_y_coord = point.y
+      point = convert_lambert93_to_gps(self.lamb_x_coord, self.lamb_y_coord)
+      self.gps_x_coord = point.long
+      self.gps_y_coord = point.lat
     
     # Create id to accelerate queries to databases
-    self.index_lamb_coord = str(self.x_lamb_coord) + str(self.y_lamb_coord)
+    self.index_lamb_coord = str(self.lamb_x_coord) + str(self.lamb_y_coord)
     super().save(*args, **kwargs)
 
   class Meta:
--- provider/tests.py
+++ provider/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from .coord_converter import convert_lambert93_to_gps
-
+from .coord_converter import convert_lambert93_to_gps, convert_gps_to_lambert93
+from .models import ProviderAvailibility
+from decimal import Decimal
 
 # Create your tests here.
 class CoordConverterTestCase(TestCase):
@@ -24,13 +25,57 @@ class CoordConverterTestCase(TestCase):
     """Function should return expected gps coordinate when given string as parameters"""
 
     # GIVEN
-    x = "102980"
-    y = "6847973"
+    x = "103113"
+    y = "6848661"
 
     #EXPECTED
-    expected_long = -5.0888561153013425
-    expected_lat = 48.4565745588153
+    expected_long = -5.088018169414728
+    expected_lat = 48.46285384827896
 
     result = convert_lambert93_to_gps(x, y)
+
     self.assertEqual(result.long, expected_long)
     self.assertEqual(result.lat, expected_lat)
+
+class ProviderAvailibilityTestCase(TestCase):
+
+  def test_index_and_gps_coord_should_be_created(self):
+    """index_lamb_coord should be concatenation of x and y lambert 93 coordinates"""
+        #From csv provided 20810;103113;6848661;1;1;0
+    ProviderAvailibility.objects.create(
+      operateur='20810',
+      lamb_x_coord=103113,
+      lamb_y_coord=6848661,
+      availibility_2G=True,
+      availibility_3G=True,
+      availibility_4G=False)
+  
+    coord = ProviderAvailibility.objects.get(index_lamb_coord='1031136848661')
+
+    self.assertEqual(coord.index_lamb_coord, '1031136848661')
+
+    #We use almost equal to workaround truncating and rounding
+    self.assertAlmostEqual(coord.gps_x_coord, Decimal(-5.088018169414728))
+    self.assertAlmostEqual(coord.gps_y_coord, Decimal(48.46285384827896))
+
+  def test_same_index_should_return_2_rows(self):
+    """We test the case when two rows have same index but different operators"""
+    ProviderAvailibility.objects.create(
+    operateur='20810',
+    lamb_x_coord=103113,
+    lamb_y_coord=6848661,
+    availibility_2G=True,
+    availibility_3G=True,
+    availibility_4G=False)
+
+    ProviderAvailibility.objects.create(
+    operateur='20815',
+    lamb_x_coord=103113,
+    lamb_y_coord=6848661,
+    availibility_2G=False,
+    availibility_3G=False,
+    availibility_4G=False)
+
+    coord = ProviderAvailibility.objects.all().filter(index_lamb_coord='1031136848661')
+    self.assertEqual(coord[0].operateur, '20810')
+    self.assertEqual(coord[1].operateur, '20815')
