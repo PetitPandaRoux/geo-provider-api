4c4448d232189ba5868f461d31330d7b4dd394aa
Sakada-Ly <sakada.ly@edifixio.com>
2021-08-31 19:27:49 +0200

    added save function

--- provider/models.py
+++ provider/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.text import slugify
 from django.contrib.auth.models import User
+from .coord_converter import convert_lambert93_to_gps
 
 # Create your models here.
 class DateModification(models.Model):
   """
   An abstract class to handle creation date and modification date 
   """
-    creation_date = models.DateTimeField(auto_now_add=True)
-    last_modification_date = models.DateTimeField(auto_now=True)
+  creation_date = models.DateTimeField(auto_now_add=True)
+  last_modification_date = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        abstract = True
+  class Meta:
+      abstract = True
 
 
 PROVIDER_CODE_NAME = (
@@ -32,15 +33,21 @@ class ProviderAvailibility(DateModification):
   lamb_y_coord = models.IntegerField()
   gps_x_coord = models.DecimalField(max_digits=18, decimal_places=16, blank=True, null=True)
   gps_y_coord = models.DecimalField(max_digits=18, decimal_places=16, blank=True, null=True)
-  index_gps_coord = models.CharField(blank=True, null=True)
-  index__lamb_coord = models.CharField(blank=True, null=True)
+  index_lamb_coord = models.CharField(max_length=255, blank=True, null=True)
   availibility_2G = models.BooleanField(default=False, verbose_name="2G availibility")
   availibility_3G = models.BooleanField(default=False, verbose_name="3G availibility")
   availibility_4G = models.BooleanField(default=False, verbose_name="4G availibility")
 
   def save(self, *args, **kwargs):
+    # Fill gps coordinate if needed
+    if(not self.gps_x_coord or not self.gps_y_coordinate):
+      point = convert_lambert_to_gps(self.x_lamb_coord, self.y_lamb_coord)
+      self.gps_x_coord = point.x
+      self.gps_y_coord = point.y
+    
+    # Create id to accelerate queries to databases
     self.index_lamb_coord = str(self.x_lamb_coord) + str(self.y_lamb_coord)
     super().save(*args, **kwargs)
 
   class Meta:
-    indexes = [models.Index(fields=['index__lamb_coord', 'index_gps_coord']), ]
+    indexes = [models.Index(fields=['index_lamb_coord', ]), ]
