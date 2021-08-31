e9f74b187c691cea13cb1bb99062dac502871bbd
Sakada-Ly <sakada.ly@edifixio.com>
2021-08-31 21:17:47 +0200

    add first endpoint and serializer view

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers 
+
+from provider.models import ProviderAvailibility
+
+class ProviderAvailibilitySerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = ProviderAvailibility
+    fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+from rest_framework.renderers import JSONRenderer
+from rest_framework import (generics, mixins, viewsets, filters)
+from provider.models import ProviderAvailibility
+from api.serializers import ProviderAvailibilitySerializer
 
+import requests
 # Create your views here.
+
+class ProviderListView(generics.ListAPIView):
+    queryset = ProviderAvailibility.objects.all()
+    serializer_class = ProviderAvailibilitySerializer
+
+def index(request):
+    return HttpResponse("Hello, world. You're at api index.")
