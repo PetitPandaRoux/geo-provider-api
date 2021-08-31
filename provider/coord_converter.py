f0c3cfc887facf75b356acb36fe24a3aa57ea94e
Sakada-Ly <sakada.ly@edifixio.com>
2021-08-31 19:27:49 +0200

    added save function

--- provider/coord_converter.py
+++ provider/coord_converter.py
@@ -1,9 +1,25 @@
 import pyproj
 from collections import namedtuple 
 
-def convert_lambert_to_gps(x: int, y: int) -> tuple:
+def convert_lambert93_to_gps(x: int, y: int) -> tuple:
   """
   It returns a named tuple of gps coordinate from lambert93 coordinate
+  """
+  lambert = pyproj.Proj('+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')
+  wgs84 = pyproj.Proj('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+  
+  #We want to return a namedtuple -> easier to manipulate
+  gps_coordinate = namedtuple('Point', 'long lat')
+ 
+  #We force type casting for x and y to int
+  long, lat = pyproj.transform(lambert, wgs84, int(x), int(y))
+  point = gps_coordinate(long, lat)
+
+  return point
+
+def convert_gps_to_lambert93(long: int, lat: int) -> tuple:
+  """
+  It returns a named tuple of lambert93 coordinate from gps coordinate
   """
   lambert = pyproj.Proj('+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')
   wgs84 = pyproj.Proj('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
@@ -12,8 +28,7 @@ def convert_lambert_to_gps(x: int, y: int) -> tuple:
   gps_coordinate = namedtuple('Point', 'x y')
  
   #We force type casting for x and y to int
-  lon, lat = pyproj.transform(lambert, wgs84, int(x), int(y))
-  point = gps_coordinate(lon, lat)
+  x, y = pyproj.transform(wgs84, lambert, int(x), int(y))
+  point = gps_coordinate(x, y)
 
   return point
-
--- provider/tests.py
+++ provider/tests.py
@@ -1,24 +1,24 @@
 from django.test import TestCase
-from .coord_converter import convert_lambert_to_gps
-from .models import ProviderAvailibility
+from .coord_converter import convert_lambert93_to_gps
+
 
 # Create your tests here.
 class CoordConverterTestCase(TestCase):
-
+  
   def test_coordinate_should_return_valid_gps(self):
-    """Function should return expected gps coordinate"""
+    """Function should return expected gps coordinates"""
 
     # GIVEN
     x = 102980
     y = 6847973
 
     #EXPECTED
-    expected_x = -5.0888561153013425
-    expected_y = 48.4565745588153
+    expected_long = -5.0888561153013425
+    expected_lat = 48.4565745588153
 
-    result = convert_lambert_to_gps(x, y)
-    self.assertEqual(result.x, expected_x)
-    self.assertEqual(result.y, expected_y)
+    result = convert_lambert93_to_gps(x, y)
+    self.assertEqual(result.long, expected_long)
+    self.assertEqual(result.lat, expected_lat)
 
   def test_coordinate_should_return_valid_gps_given_string(self):
     """Function should return expected gps coordinate when given string as parameters"""
@@ -28,9 +28,9 @@ class CoordConverterTestCase(TestCase):
     y = "6847973"
 
     #EXPECTED
-    expected_x = -5.0888561153013425
-    expected_y = 48.4565745588153
+    expected_long = -5.0888561153013425
+    expected_lat = 48.4565745588153
 
-    result = convert_lambert_to_gps(x, y)
-    self.assertEqual(result.x, expected_x)
-    self.assertEqual(result.y, expected_y)
+    result = convert_lambert93_to_gps(x, y)
+    self.assertEqual(result.long, expected_long)
+    self.assertEqual(result.lat, expected_lat)
